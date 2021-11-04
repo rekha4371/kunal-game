e3175d9086efb315b171a7766bd1746bcaf318f4
rekha4371 <76061600+rekha4371@users.noreply.github.com>
2021-11-04 20:47:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var bikes, bike1, bike2;
+
+var track, bike1_img, bike2_img;
+
+function preload(){
+  track = loadImage("images/track.png");
+  bike1_img = loadImage("images/bike1.png");
+  bike2_img = loadImage("images/bike2.png");
+ 
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
